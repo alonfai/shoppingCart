2902779d3a820d3023705d3306a9d82728159972
Alon Faingertz <alonfai@gmail.com>
2017-07-13 13:52:14 +1000

    update main file with sample results

--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
-// import shoppingCart from './shoppingCart'
-// import {offers} from './model'
-//
-// const cart = shoppingCart.new(offers);
+/*eslint-disable no-console*/
+import shoppingCart from './shoppingCart'
+import {offers} from './model'
 
 //Scenario 1
 //----------
-// cart.add('ult_small', 3)
-// cart.add('ult_large', 1)
+const cart1 = shoppingCart.new(offers);
+cart1.add('ult_small', 3)
+cart1.add('ult_large', 1)
+cart1.items
+cart1.total
+console.log('Cart 1');
+console.log('------');
+console.log(cart1.items);
+console.log(`total value = ${cart1.total}`);
 
 //Scenario 2
 //----------
-// cart.add('ult_small', 2)
-// cart.add('ult_large', 4)
+const cart2 = shoppingCart.new(offers);
+cart2.add('ult_small', 2)
+cart2.add('ult_large', 4)
+console.log('Cart 2');
+console.log('------');
+console.log(cart2.items);
+console.log(`total value = ${cart2.total}`);
 
 //Scenario 3
 //----------
-// cart.add('ult_small', 1)
-// cart.add('ult_medium', 2)
+const cart3 = shoppingCart.new(offers);
+cart3.add('ult_small', 1)
+cart3.add('ult_medium', 2)
+console.log('Cart 3');
+console.log('------');
+console.log(cart3.items);
+console.log(`total value = ${cart3.total}`);
 
 //Scenario 4
 //----------
-// cart.add('ult_small', 1)
-// cart.add('1gb', 1)
-// cart.addPromoCode('I<3AMAYSIM')
-
-// cart.add('ult_large', 1)
-// cart.updateQuantity('ult_large', 4)
-
-// cart.items()
-// cart.total()
+const cart4 = shoppingCart.new(offers);
+cart4.add('ult_small', 1)
+cart4.add('1gb', 1)
+cart4.addPromoCode('I<3AMAYSIM')
+console.log('Cart 4');
+console.log('------');
+console.log(cart4.items);
+console.log(`total value = ${cart4.total}`);
